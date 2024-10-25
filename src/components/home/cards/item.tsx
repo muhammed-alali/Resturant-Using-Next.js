2236641d1216aee74dcb0d95277d12c35c59d256
muhammed-alali <elastura444@gmail.com>
2024-10-25 21:53:06 +0300

    new abdete

--- src/components/home/cards/item.tsx
+++ src/components/home/cards/item.tsx
@@ -10,9 +10,9 @@ export let cardItems = [
     addCard: "Add To Cart",
     images: [
       "https://foodking-react.vercel.app/assets/img/food/pasta-2.png",
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQmIpCAk-9rNJVNA2GH_3Xb3yMN5ndlQeCZIgftlB719hHP3TOp",
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT7Evvofu2KgCGXHAZm4p7Q4hZiMYoh1ydIeQjQv3p3orNb23NJ",
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQUCHNt0oY7lzrJFdya7_TChQ8ZJn7Zh8fpPyI7YZWHNqmrRtv0",
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTgfdsApQNMi1T7dj6OVro-ItDPdFt48BzcYol05ZGL7YDahQM1",
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTKsPmj4i9LOC_HePZrVPQan3vyxmNa3yb5BbJ6rmw8L7zVvLUW",
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT5SovoLY5qvhsRkcP5t0uRP--9Sm-8dG7ER8RmWmDu4KCRfKzi",
     ],
   },
   {
@@ -74,13 +74,13 @@ export let cardItems = [
     addCard: "Add To Cart",
     images: [
       "https://foodking-react.vercel.app/assets/img/food/pizza-3.png",
-      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR_3MyQNyC5sjoc6UE6WQYoFMyqQlZUwfRVGczQasL1ipuoFJPU",
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRA7KSwPccZRYDy57ntNPM2hBCWoQYA4OMOcdA1HzN18tznMazx",
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQMcKrSSzsrcJsFqAlOf4CeFz_v-UblkS_v_886xRaNtj1Lr-se",
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSkrtwk8SELLoccYwKybiff-rAt1GA6iIUFQfDWn0ZFPNY5joOh",
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTritFU3RQqcbyds1rFJ1wVYnuS07tiHGAhaNpOaPCgqAvBdM4g",
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQK9Tk1oI3Yrcbc01vtokkj1x2yoR6Xm4NmLURCy4alzLWJIdz8",
     ],
   },
   {
-    id: 5,
+    id: 6,
     url: "https://foodking-react.vercel.app/assets/img/food/beef-ruti.png",
     price: 86,
     title: "Ruti Rith Beef Slice",
@@ -90,9 +90,9 @@ export let cardItems = [
     addCard: "Add To Cart",
     images: [
       "https://foodking-react.vercel.app/assets/img/food/beef-ruti.png",
-      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR_3MyQNyC5sjoc6UE6WQYoFMyqQlZUwfRVGczQasL1ipuoFJPU",
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRA7KSwPccZRYDy57ntNPM2hBCWoQYA4OMOcdA1HzN18tznMazx",
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQMcKrSSzsrcJsFqAlOf4CeFz_v-UblkS_v_886xRaNtj1Lr-se",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZcoKRdH-5xNANLXr1h-6L54uTleNivNHZ6QFHETphAH-Kgft5",
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcROerT74Wh5R9IFW3fy-q6WF9N36RNHXHyvzofyczlZRGuF-M4F",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsqbBPwEXYLbPTWEMeYVkz2ieYUL6js29szZqUr7NWK41lrfR",
     ],
   },
 ];
